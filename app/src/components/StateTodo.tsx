6eaf5bae14ec756d5fb42d1bdc6be2fd6c139c55
Isamu Suzuki <isamusuzuki@outlook.com>
2025-04-30 01:33:09 +0000

    update

--- app/src/components/StateTodo.tsx
+++ app/src/components/StateTodo.tsx
@@ -13,6 +13,7 @@ export default function StateTodo() {
 
     const [title, setTitle] = useState<string>('');
     const [todos, setTodos] = useState<iTodo[]>([]);
+    const [desc, setDesc] = useState<boolean>(false);
 
     const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTitle(e.target.value);
@@ -29,13 +30,31 @@ export default function StateTodo() {
     };
 
     const handleDone = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const thisId = Number(e.currentTarget.dataset.id);
         setTodos(todos.map(todo => {
-            const numberId = Number(e.currentTarget.dataset.id);
-            if (todo.id === numberId) {
+            if (todo.id === thisId) {
                 return { ...todo, isDone: !todo.isDone };
             }
             return todo;
         }));
+    };
+
+    const handleRemove = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const thisId = Number(e.currentTarget.dataset.id);
+        setTodos(todos.filter(todo => todo.id !== thisId));
+    };
+
+    const handleSort = () => {
+        setTodos(todos.sort((a, b) => {
+            if (desc) {
+                // createdAtの降順でソートする
+                return b.createdAt.getTime() - a.createdAt.getTime();
+            } else {
+                // createdAtの昇順でソートする
+                return a.createdAt.getTime() - b.createdAt.getTime();
+            }
+        }));
+        setDesc(!desc);
     };
 
     return (
@@ -48,13 +67,17 @@ export default function StateTodo() {
                         value={title}
                         onChange={handleChangeTitle}
                         placeholder="Add a new todo"
-                        className='border bg-white rounded p-2 max-w-lg mr-2'
+                        className='border bg-white rounded p-2 w-70 mr-2'
                     />
                 </label>
                 <button
                     onClick={handleClick}
-                    className='bg-blue-500 text-white px-5 py-2 rounded-md'
+                    className='bg-blue-500 hover:bg-blue-700 text-white px-5 py-2 rounded-md mr-2'
                 >追加</button>
+                <button
+                    onClick={handleSort}
+                    className='bg-gray-500 hover:bg-gray-700 text-white px-5 py-2 rounded-md'
+                >ソート ({desc ? '↑' : '↓'})</button>
             </div>
             <ul className='list-disc list-inside p-2'>
                 {todos.map(todo => (
@@ -63,7 +86,13 @@ export default function StateTodo() {
                         <button type="button"
                             data-id={todo.id}
                             onClick={handleDone}
-                            className='ml-2 mb-2 bg-green-500 text-white px-2 py-1 rounded-md'>済</button>
+                            className='ml-2 mb-2 bg-green-500 hover:bg-green-700 text-white px-2 py-1 rounded-md'
+                        >済</button>
+                        <button type="button"
+                            data-id={todo.id}
+                            onClick={handleRemove}
+                            className='ml-2 mb-2 bg-red-500 hover:bg-red-700 text-white px-2 py-1 rounded-md'
+                        >削除</button>
                     </li>
                 ))}
             </ul>
